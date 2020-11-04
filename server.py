119e72e22b8d564d4b26e9700e9e2d98f92fc8fd
mavroian <mavflorin@gmail.com>
2020-11-04 09:26:20 +0900

    added enpoint for project template

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 import csv
+import json
+from operator import itemgetter
 from flask import Flask, render_template , request , redirect
 app = Flask(__name__)
 
@@ -8,7 +10,24 @@ def home():
 
 @app.route('/<string:page_name>')
 def html_page(page_name):
+    print(page_name)
     return render_template(page_name)
+
+def get_work_data(id):
+    with open('templates/projects/myworks.json') as f:
+        data = json.load(f)
+        result = filter(lambda work_id: work_id['id'] == id, data)
+        return list(result)
+
+@app.route('/projects/<string:page_name>')
+
+def projects_page(page_name):
+    query_param =  {k:v for k, v in request.args.items()}
+    project_id = query_param['id']
+    project = get_work_data(project_id)
+    title = project[0]['title']
+    return render_template(f'projects/{page_name}', project=project)
+
 
 def write_to_file(data):
     with open('database.txt', mode='a') as db:
@@ -16,7 +35,7 @@ def write_to_file(data):
         subject = data['subject']
         message = data['message']
         name= data['name']
-        file = db.write(f'\n{name},{email},{subject},{message}')
+        db.write(f'\n{name},{email},{subject},{message}')
 
 def write_to_csv(data):
     with open('database.csv', mode='a' ,newline='') as db2:
