abec320db586aa3df65da6a8221e7d27fad28ea9
mavroian <mavflorin@gmail.com>
2020-11-09 14:38:31 +0900

    update

--- server.py
+++ server.py
@@ -22,10 +22,10 @@ def get_work_data(id):
 @app.route('/projects/<string:page_name>')
 
 def projects_page(page_name):
-    # query_param =  {k:v for k, v in request.args.items()}
-    # project_id = query_param['id']
-    # project = get_work_data(project_id)
-    return render_template(f'projects/{page_name}', project={"title":'hello'})
+    query_param =  {k:v for k, v in request.args.items()}
+    project_id = query_param['id']
+    project = get_work_data(project_id)
+    return render_template(f'projects/{page_name}', project=project)
 
 
 def write_to_file(data):
