feb570d78988c27bb9b036329b74fc7c1b5ec6ae
mavroian <mavflorin@gmail.com>
2020-12-02 11:56:40 +0900

    updated server added error handling

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import csv
 import os
 from operator import itemgetter
 from flask import Flask, render_template , request , redirect, url_for, json
+from jinja2 import TemplateNotFound
 
 app = Flask(__name__)
 
@@ -11,8 +12,18 @@ def home():
 
 @app.route('/<string:page_name>')
 def html_page(page_name):
-    print(page_name)
-    return render_template(f'{page_name}.html')
+    try:
+        if '.html' in page_name:
+            return render_template(page_name)
+        return render_template(f'{page_name}.html')
+    except TemplateNotFound:
+        return render_template('404.html')
+        
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
 
 def showjson(id):
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
@@ -26,6 +37,9 @@ def projects_page(page_name):
     query_param =  {k:v for k, v in request.args.items()}
     p_id = query_param['id']
     work = showjson(p_id)
+    if '.html' in page_name:
+        return render_template(f'projects/{page_name}', project=work[0])
+
     return render_template(f'projects/{page_name}.html', project=work[0])
 
 
