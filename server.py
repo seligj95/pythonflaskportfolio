bfeec4a8f9a369acc09a130405245a4208d63dee
mavroian <mavflorin@gmail.com>
2020-12-03 22:22:30 +0900

    added sitemap

--- server.py
+++ server.py
@@ -15,6 +15,8 @@ def html_page(page_name):
     try:
         if '.html' in page_name:
             return render_template(page_name)
+        elif '.xml' in page_name:
+            return render_template('sitemap.xml')
         return render_template(f'{page_name}.html')
     except TemplateNotFound:
         return render_template('404.html')
